b7c05dbb4b0b5d9c9ed7da7d541a89f46acfb54e
Jackie Chui <jachui@microsoft.com>
2018-11-15 15:12:51 -0800

    removed comments

--- src/mutationObserver.js
+++ src/mutationObserver.js
@@ -41,7 +41,7 @@ export function startMutationObserver() {
 			);
 		}
 
-		if (!document.querySelector('div[class*="modal"]') && modalOpened) {
+		if (!document.querySelector('div[class*="modal--header--"]') && modalOpened) {
 			modalOpened = false;
 			window.dispatchEvent(new CustomEvent('modalClosed'));
 		}
@@ -67,95 +67,3 @@ export function startMutationObserver() {
 		}
 	}).observe(document.getElementById('react-page'), { childList: true, subtree: true });
 }
-
-// export function startMutationObserver() {
-// 	let fileBrowserLoaded = false;
-// 	let fileLoaded = false;
-// 	let menuOpened = false;
-// 	new MutationObserver(mutations => {
-// 		for (let i = 0; i < mutations.length; i++) {
-// 			const mutation = mutations[i];
-// 			if (document.querySelector('[data-tooltip-text="Show notifications"]') !== null && !fileBrowserLoaded) {
-// 				fileBrowserLoaded = true;
-// 				window.dispatchEvent(new CustomEvent('fileBrowserLoaded'));
-// 			}
-// 			if (document.querySelector('[data-tooltip-text="Show notifications"]') === null && fileBrowserLoaded) {
-// 				fileBrowserLoaded = false;
-// 				window.dispatchEvent(new CustomEvent('fileBrowserUnloaded'));
-// 			}
-// 			if (mutation.addedNodes[0] === document.querySelector('div[class*="top_bar--header--"]')) {
-// 				window.dispatchEvent(new CustomEvent('fileBrowserChanged'));
-// 			}
-// 			if (window.App._fullscreenIsReady && window.App._state.selectedView.fullscreen && !fileLoaded) {
-// 				fileLoaded = true;
-// 				window.dispatchEvent(new CustomEvent('fileLoaded'));
-// 			}
-// 			if (!window.App._state.selectedView.fullscreen && fileLoaded) {
-// 				fileLoaded = false;
-// 				window.dispatchEvent(new CustomEvent('fileUnloaded'));
-// 			}
-// 			if (
-// 				mutation.addedNodes[0] &&
-// 				mutation.addedNodes[0].className &&
-// 				typeof mutation.addedNodes[0].className === 'string' &&
-// 				mutation.addedNodes[0].className.includes('modal--')
-// 			) {
-// 				window.dispatchEvent(
-// 					new CustomEvent('modalOpened', {
-// 						detail: document.querySelector('div[class*="modal--header"]').innerText
-// 					})
-// 				);
-// 			}
-// 			if (
-// 				mutation.removedNodes[0] &&
-// 				mutation.removedNodes[0].className &&
-// 				typeof mutation.removedNodes[0].className === 'string' &&
-// 				mutation.removedNodes[0].className.includes('modal--')
-// 			) {
-// 				window.dispatchEvent(new CustomEvent('modalClosed'));
-// 			}
-// 			if (window.App._state.dropdownShown !== null && !menuOpened) {
-// 				menuOpened = true;
-// 				window.dispatchEvent(
-// 					new CustomEvent('menuOpened', {
-// 						detail: {
-// 							menu: window.App._state.dropdownShown,
-// 							hasMoreOptions:
-// 								[...document.querySelectorAll('div[class*="multilevel_dropdown--name"]')].find(
-// 									node => node.innerText === 'More'
-// 								) !== undefined
-// 						}
-// 					})
-// 				);
-// 			}
-// 			if (!window.App._state.dropdownShown && menuOpened) {
-// 				menuOpened = false;
-// 				window.dispatchEvent(new CustomEvent('menuClosed'));
-// 			}
-// 			if (
-// 				mutation.addedNodes[0] &&
-// 				mutation.addedNodes[0].attributes &&
-// 				mutation.addedNodes[0].attributes.name &&
-// 				mutation.addedNodes[0].attributes.name.value === 'layerMenu' &&
-// 				document.querySelectorAll('div[name="layerMenu"]').length === 2 &&
-// 				[
-// 					...[...document.querySelectorAll('div[class*="multilevel_dropdown--name"]')].find(
-// 						node => node.innerText === 'More'
-// 					).parentNode.classList
-// 				].some(className => className.includes('dropdown--optionActive'))
-// 			) {
-// 				window.dispatchEvent(
-// 					new CustomEvent('moreMenuOpened', {
-// 						detail: {
-// 							menu: window.App._state.dropdownShown,
-// 							hasMoreOptions:
-// 								[...document.querySelectorAll('div[class*="multilevel_dropdown--name"]')].find(
-// 									node => node.innerText === 'More'
-// 								) !== undefined
-// 						}
-// 					})
-// 				);
-// 			}
-// 		}
-// 	}).observe(document.getElementById('react-page'), { childList: true, subtree: true });
-// }
