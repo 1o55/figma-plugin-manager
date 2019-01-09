f65dfb62a237eafa04c33bec847d4d910135f3e7
Jackie Chui <jachui@microsoft.com>
2019-01-09 09:41:13 -0800

    Added getNode and getScene functions

--- src/FigmaPluginAPI.js
+++ src/FigmaPluginAPI.js
@@ -154,29 +154,15 @@ export const FigmaPluginAPI = {
 			: null;
 		window.App._dispatch(toast);
 	},
-	forEachSelectedNode: triggerFunction => {
-		const selectedNodes = Object.keys(App._state.mirror.sceneGraphSelection);
-		for (let i = 0; i < selectedNodes.length; i++) {
-			const node = selectedNodes[i];
-			App.sendMessage('clearSelection');
-			App.sendMessage('addToSelection', { nodeIds: [node] });
-			triggerFunction(node);
-		}
-		App.sendMessage('addToSelection', { nodeIds: selectedNodes });
+	getNode: node => {
+		return getNode(node);
 	},
-	replaceText: text => {
-		App.triggerAction('request-edit-mode');
-		var inputNode = document.getElementsByClassName('focus-target')[0];
-		inputNode.focus();
-		inputNode.value = text;
-		inputNode.dispatchEvent(new InputEvent('input'));
-		App.triggerAction('leave-edit-mode');
-	},
-	getNodeType: node => {
-		return App._state.mirror.sceneGraph.get(node).type;
-	},
-	getTextContent: node => {
-		return App.sendMessage('inspectNodeForInteractionTests', { nodeId: node }).args.extractedText;
+	getScene: () => {
+		return {
+			root: getNode('0:0'),
+			currentPage: getNode(App._state.mirror.appModel.currentPage),
+			selection: Object.keys(App._state.mirror.sceneGraphSelection).map(nodeId => getNode(nodeId))
+		};
 	}
 };
 
@@ -381,4 +367,362 @@ const addKeyboardShortcutInFile = (focusTarget, shortcut, triggerFunction) => {
 			triggerFunction();
 		}
 	});
+};
+
+const getNode = node => {
+	const sceneNode = App._state.mirror.sceneGraph.get(node);
+	const childrenGetter = sceneNode.__lookupGetter__('children');
+	Object.defineProperties(sceneNode.__proto__, {
+		children: {
+			get: childrenGetter,
+			set: function(val) {
+				Object.defineProperty(this, 'children', { value: val, writable: true });
+			}
+		}
+	});
+	sceneNode.children = sceneNode.children.map(child => {
+		return getNode(child);
+	});
+	const newNode = { id: sceneNode.guid, type: sceneNode.type };
+	if (sceneNode.parent) {
+		Object.defineProperty(newNode, 'parent', {
+			get: function() {
+				return getNode(App, sceneNode.parent);
+			}
+		});
+	}
+	Object.defineProperties(newNode, {
+		name: {
+			get: function() {
+				return sceneNode.name;
+			},
+			set: function(val) {
+				App.sendMessage('setNodeProperty', {
+					nodeId: sceneNode.guid,
+					property: 'name',
+					value: val
+				});
+			}
+		},
+		visible: {
+			get: function() {
+				return sceneNode.visible;
+			},
+			set: function(val) {
+				App.sendMessage('setNodeProperty', {
+					nodeId: sceneNode.guid,
+					property: 'visible',
+					value: val
+				});
+			}
+		},
+		locked: {
+			get: function() {
+				return sceneNode.locked;
+			},
+			set: function(val) {
+				App.sendMessage('setNodeProperty', {
+					nodeId: sceneNode.guid,
+					property: 'locked',
+					value: val
+				});
+			}
+		}
+	});
+	if (sceneNode.children.length !== 0) {
+		newNode.children = sceneNode.children;
+	}
+	if (!(sceneNode.type === 'DOCUMENT' || sceneNode.type === 'CANVAS')) {
+		const moreInfo = App.sendMessage('inspectNodeForInteractionTests', { nodeId: sceneNode.guid }).args;
+		newNode.size = moreInfo.size;
+		newNode.absoluteBounds = moreInfo.absoluteBounds;
+		const relativeTransform = moreInfo.relativeTransform;
+		newNode.relativeTransform = [
+			[relativeTransform.m00, relativeTransform.m01, relativeTransform.m02],
+			[relativeTransform.m10, relativeTransform.m11, relativeTransform.m12]
+		];
+		if (sceneNode.type === 'TEXT') {
+			Object.defineProperty(newNode, 'characters', {
+				get: function() {
+					return moreInfo.extractedText;
+				},
+				set: function(val) {
+					const selectedNodes = Object.keys(App._state.mirror.sceneGraphSelection);
+					App.sendMessage('clearSelection');
+					App.sendMessage('addToSelection', { nodeIds: [newNode.id] });
+					App.triggerAction('request-edit-mode');
+					var inputNode = document.querySelector('.focus-target');
+					inputNode.focus();
+					inputNode.value = val;
+					inputNode.dispatchEvent(new InputEvent('input'));
+					App.triggerAction('leave-edit-mode');
+					App.sendMessage('clearSelection');
+					if (selectedNodes.length > 0) App.sendMessage('addToSelection', { nodeIds: selectedNodes });
+				}
+			});
+		}
+		newNode.getProperties = function(callback) {
+			if (!(newNode.type === 'DOCUMENT' || newNode.type === 'CANVAS')) {
+				const selectedNodes = Object.keys(App._state.mirror.sceneGraphSelection);
+				App.sendMessage('clearSelection');
+				App.sendMessage('addToSelection', { nodeIds: [newNode.id] });
+				setTimeout(() => {
+					const result = App._state.mirror.selectionProperties;
+					Object.defineProperties(newNode, {
+						opacity: {
+							get: function() {
+								return result.opacity;
+							},
+							set: function(val) {
+								App.updateSelectionProperties({ opacity: val });
+							}
+						},
+						blendMode: {
+							get: function() {
+								return result.blendMode;
+							},
+							set: function(val) {
+								App.updateSelectionProperties({ blendMode: val });
+							}
+						},
+						isMask: {
+							get: function() {
+								return result.mask;
+							},
+							set: function(val) {
+								App.updateSelectionProperties({ mask: val });
+							}
+						},
+						effects: {
+							get: function() {
+								return result.effects;
+							},
+							set: function(val) {
+								App.updateSelectionProperties({ effects: val });
+							}
+						},
+						constraints: {
+							get: function() {
+								return { horizontal: result.horizontalConstraint, vertical: result.verticalConstraint };
+							},
+							set: function(val) {
+								App.updateSelectionProperties({ horizontalConstraint: val.horizontal });
+								App.updateSelectionProperties({ verticalConstraint: val.vertical });
+							}
+						},
+						x: {
+							get: function() {
+								return result.x;
+							},
+							set: function(val) {
+								App.updateSelectionProperties({ x: val });
+							}
+						},
+						y: {
+							get: function() {
+								return result.y;
+							},
+							set: function(val) {
+								App.updateSelectionProperties({ y: val });
+							}
+						},
+						width: {
+							get: function() {
+								return result.width;
+							},
+							set: function(val) {
+								App.updateSelectionProperties({ width: val });
+							}
+						},
+						height: {
+							get: function() {
+								return result.height;
+							},
+							set: function(val) {
+								App.updateSelectionProperties({ height: val });
+							}
+						}
+					});
+					if (newNode.type !== 'FRAME' || newNode.type !== 'INSTANCE') {
+						Object.defineProperties(newNode, {
+							fills: {
+								get: function() {
+									return result.fillPaints;
+								},
+								set: function(val) {
+									App.updateSelectionProperties({ fillPaints: val });
+								}
+							},
+							strokes: {
+								get: function() {
+									return result.fillPaints;
+								},
+								set: function(val) {
+									App.updateSelectionProperties({ fillPaints: val });
+								}
+							},
+							strokeWeight: {
+								get: function() {
+									return result.strokeWeight;
+								},
+								set: function(val) {
+									App.updateSelectionProperties({ strokeWeight: val });
+								}
+							},
+							strokeAlign: {
+								get: function() {
+									return result.strokeAlign;
+								},
+								set: function(val) {
+									App.updateSelectionProperties({ strokeAlign: val });
+								}
+							}
+						});
+					}
+					if (newNode.type === 'FRAME' || newNode.type === 'INSTANCE') {
+						Object.defineProperties(newNode, {
+							backgrounds: {
+								get: function() {
+									return result.backgroundPaints;
+								},
+								set: function(val) {
+									App.updateSelectionProperties({ backgroundPaints: val });
+								}
+							},
+							clipsContent: {
+								get: function() {
+									return !result.frameMaskDisabled;
+								},
+								set: function(val) {
+									App.updateSelectionProperties({ frameMaskDisabled: !val });
+								}
+							}
+						});
+					}
+					if (
+						newNode.type === 'BOOLEAN_OPERATION' ||
+						newNode.type === 'VECTOR' ||
+						newNode.type === 'STAR' ||
+						newNode.type === 'REGULAR_POLYGON' ||
+						newNode.type === 'RECTANGLE'
+					) {
+						Object.defineProperty(newNode, 'cornerRadius', {
+							get: function() {
+								return result.cornerRadius;
+							},
+							set: function(val) {
+								App.updateSelectionProperties({ cornerRadius: val });
+							}
+						});
+					}
+					switch (newNode.type) {
+						case 'STAR':
+							Object.defineProperties(newNode, {
+								pointCount: {
+									get: function() {
+										return result.count;
+									},
+									set: function(val) {
+										App.updateSelectionProperties({ count: val });
+									}
+								},
+								starInnerRadius: {
+									get: function() {
+										return result.starInnerScale;
+									},
+									set: function(val) {
+										App.updateSelectionProperties({ starInnerScale: val });
+									}
+								}
+							});
+							break;
+						case 'ELLIPSE':
+							Object.defineProperty(newNode, 'arcData', {
+								get: function() {
+									return {
+										startingAngle: result.arcStart,
+										endingAngle: result.arcSweep,
+										innerRadius: result.arcRadius
+									};
+								},
+								set: function(val) {
+									App.updateSelectionProperties({ arcStart: startingAngle });
+									App.updateSelectionProperties({ arcSweep: endingAngle });
+									App.updateSelectionProperties({ arcRadius: innerRadius });
+								}
+							});
+							break;
+						case 'REGULAR_POLYGON':
+							Object.defineProperty(newNode, 'pointCount', {
+								get: function() {
+									return result.count;
+								},
+								set: function(val) {
+									App.updateSelectionProperties({ count: val });
+								}
+							});
+							break;
+						case 'TEXT':
+							Object.defineProperties(newNode, {
+								fontSize: {
+									get: function() {
+										return result.fontSize;
+									},
+									set: function(val) {
+										App.updateSelectionProperties({ fontSize: val });
+									}
+								},
+								textAlignHorizontal: {
+									get: function() {
+										return result.textAlignHorizontal;
+									},
+									set: function(val) {
+										App.updateSelectionProperties({ textAlignHorizontal: val });
+									}
+								},
+								textAlignVertical: {
+									get: function() {
+										return result.textAlignVertical;
+									},
+									set: function(val) {
+										App.updateSelectionProperties({ textAlignVertical: val });
+									}
+								},
+								textDecoration: {
+									get: function() {
+										return result.textDecoration;
+									},
+									set: function(val) {
+										App.updateSelectionProperties({ textDecoration: val });
+									}
+								},
+								textAutoResize: {
+									get: function() {
+										return result.textAutoResize;
+									},
+									set: function(val) {
+										App.updateSelectionProperties({ textAutoResize: val });
+									}
+								},
+								fontName: {
+									get: function() {
+										return result.fontName;
+									},
+									set: function(val) {
+										App.updateSelectionProperties({ fontName: val });
+									}
+								}
+							});
+					}
+					if (callback !== undefined) callback(newNode);
+					App.sendMessage('clearSelection');
+					if (selectedNodes.length > 0) App.sendMessage('addToSelection', { nodeIds: selectedNodes });
+				}, 16);
+			}
+		};
+	}
+	if (sceneNode.type === 'BOOLEAN_OPERATION') {
+		newNode.booleanOperation = sceneNode.booleanOperation;
+	}
+	return newNode;
 };
